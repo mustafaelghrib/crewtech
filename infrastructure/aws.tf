a26c431284fe5c6af428195f42e4368789bce5d2
mstva <mustafa.abdulluh@gmail.com>
2022-12-08 14:57:09 +0200

    added AWS RDS postgres database

--- infrastructure/aws.tf
+++ infrastructure/aws.tf
@@ -13,6 +13,8 @@ provider "aws" {
   secret_key = var.aws.secret_key
 }
 
+variable "database_password" { type = string }
+
 module "aws" {
   source = "./modules/aws"
 
@@ -20,6 +22,17 @@ module "aws" {
   bucket_secret_key = var.aws.secret_key
   bucket_name       = "${local.project_name}-bucket"
   bucket_acl        = "private"
+
+  database_identifier        = "${local.project_name}-database"
+  database_name              = "${local.project_name}_db"
+  database_username          = "${local.project_name}_user"
+  database_password          = var.database_password
+  database_port              = 5432
+  database_engine            = "postgres"
+  database_engine_version    = "13.7"
+  database_instance_class    = "db.t3.micro"
+  database_storage_type      = "gp2"
+  database_allocated_storage = "20"
 
 }
 
