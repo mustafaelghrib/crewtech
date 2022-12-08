02890bd5ff02055e00eaf2bc2aaf4ab7ed4afd54
mstva <mustafa.abdulluh@gmail.com>
2022-12-08 15:08:00 +0200

    added the production environment

--- README.md
+++ README.md
@@ -27,6 +27,17 @@ A full production backend API built with these tech stacks:
   docker exec -it crewtech_base_django /bin/bash -c "/opt/venv/bin/pytest"
   ```
 
+**Run the production environment locally:**
+- Get the environment variables from the infrastructure:
+  ```shell
+  python scripts/get_infra_output.py --c=infrastructure/docker-compose.yml --m=aws --f=env
+  ```
+- Update the `backend/.env/.env.production` file.
+- Run Docker Compose:
+  ```shell
+  docker compose -f backend/.docker-compose/production.yml up -d --build
+  ```
+
 ---
 
 ### Infrastructure:
---
+++ backend/src/settings/production.py
@@ -0,0 +1,25 @@
+from .base import *
+
+DEBUG = os.environ.get("DJANGO_DEBUG", default=False)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_HOST"),
+        'PORT': os.environ.get("POSTGRES_PORT"),
+    }
+}
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_BUCKET_DOMAIN_NAME = os.environ.get('AWS_BUCKET_DOMAIN_NAME')
+
+STATIC_URL = f'https://{AWS_BUCKET_DOMAIN_NAME}/static/'
+STATICFILES_STORAGE = 'src.storage.StaticStorage'
+
+MEDIA_URL = f'https://{AWS_BUCKET_DOMAIN_NAME}/media/'
+DEFAULT_FILE_STORAGE = 'src.storage.MediaStorage'
